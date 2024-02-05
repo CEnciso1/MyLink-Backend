172dbc8e835ffa64e31dce2b66a95ea1cd8c8859
CEnciso1 <89319860+CEnciso1@users.noreply.github.com>
2024-02-05 17:41:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 //cors allows client to make request to server
 app.use(
   cors({
-    origin: ["https://mylink-frontend.onrender.com","http://localhost:5173/", "http://localhost:3000/"],
+    origin: "http://localhost:3000/" , //"https://mylink-frontend.onrender.com"
     credentials: true,
   })
 );
